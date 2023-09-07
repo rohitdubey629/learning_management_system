dac5e7a95ebf34f457b3219792d8b0bb75af829f
rohitdubey629 <dubeyrohit629@gmail.con>
2023-09-07 11:39:45 +0530

    Added basic layout and add to gomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <Footer />
+      <Routes>
+        <Route path="" element={<HomePage />}></Route>
+      </Routes>
     </>
   );
 }
