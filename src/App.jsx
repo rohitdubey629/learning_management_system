42891f16e40ae9934497baed5d5b031ad5a62e6e
rohitdubey629 <dubeyrohit629@gmail.con>
2023-12-15 13:36:11 +0530

    added complete sognup page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import "./App.css";
 import AboutUs from "./pages/AboutUs";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import Signum from "./pages/Signup";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="" element={<HomePage />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
-
+        <Route path="/signup" element={<Signum />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </>
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,192 @@
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { BsPersonCircle } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { createAccount } from "../Redux/Slices/AuthSlice";
+import HomeLayout from "../layout/HomeLayout";
+
+const Signup = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [previewImage, setPreviewImage] = useState("");
+
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    avatar: "",
+  });
+
+  const handelUserInput = (e) => {
+    const { name, value } = e.target;
+    setSignupData({
+      ...signupData,
+      [name]: value,
+    });
+  };
+
+  const getImage = (event) => {
+    event.preventDefault();
+    const uploadedImage = event.target.files[0];
+
+    if (uploadedImage) {
+      setSignupData({
+        ...signupData,
+        avatar: uploadedImage,
+      });
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(uploadedImage);
+      fileReader.addEventListener("load", function () {
+        setPreviewImage(this.result);
+      });
+    }
+  };
+
+  const createNewAccount = async (event) => {
+    event.preventDefault();
+    if (
+      !signupData.email ||
+      !signupData.password ||
+      !signupData.fullName ||
+      !signupData.avatar
+    ) {
+      toast.error("Please fill all the detail");
+      return;
+    }
+
+    //checking valid email
+    if (
+      !signupData.email.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+    ) {
+      toast.error("please enter a valid email address");
+      return;
+    }
+    //checking valid password
+    if (
+      !signupData.password.match(
+        /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
+      )
+    ) {
+      toast.error(
+        "Password should contain at least one digit and one special character"
+      );
+      return;
+    }
+    console.log("res");
+
+    const formData = new FormData();
+    formData.append("fullName", signupData.fullName);
+    formData.append("email", signupData.email);
+    formData.append("password", signupData.password);
+    formData.append("avatar", signupData.avatar);
+
+    //dispatch create account
+    const response = await dispatch(createAccount(formData));
+    if (response?.payload?.success) navigate("/");
+
+    setSignupData({
+      fullName: "",
+      email: "",
+      password: "",
+      avatar: "",
+    });
+    setPreviewImage("");
+  };
+
+  return (
+    <HomeLayout>
+      <div className="flex items-center justify-center h-[100vh]">
+        <form
+          noValidate
+          onSubmit={createNewAccount}
+          className="flex flex-col justify-center gap-3 rounded-lg p-4 text-white w-96 shadow-[0_0_10px_black]"
+        >
+          <h1 className="text-center text-2xl font-bold">Registration Page</h1>
+
+          <label htmlFor="image_uploads" className="cursor-pointer">
+            {previewImage ? (
+              <img
+                src={previewImage}
+                className="w-24 h-24 rounded-full m-auto"
+              />
+            ) : (
+              <BsPersonCircle className="w-24 h-24 rounded-full m-auto" />
+            )}
+          </label>
+
+          <input
+            className="hidden"
+            type="file"
+            name="image_uploads"
+            id="image_uploads"
+            accept=".jpg, .jpeg, .png, .svg "
+            onChange={getImage}
+          />
+          <div className="flex flex-col gap-1">
+            <label htmlFor="fullName" className="font-semibold">
+              Name
+            </label>
+            <input
+              type="text"
+              name="fullName"
+              id="fullName"
+              required
+              placeholder="Enter your name"
+              className="bg-transparent px-2 py-1 border"
+              onChange={handelUserInput}
+              value={signupData.fullName}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email" className="font-semibold">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              placeholder="Enter your email"
+              className="bg-transparent px-2 py-1 border"
+              onChange={handelUserInput}
+              value={signupData.email}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password" className="font-semibold">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              required
+              placeholder="Enter your password..."
+              className="bg-transparent px-2 py-1 border"
+              onChange={handelUserInput}
+              value={signupData.password}
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-yellow-600 hover:bg-yellow-500 transition-all ease-in-out duration-300 rounded-sm py-2 font-bold text-xl cursor-pinter"
+          >
+            Create Account
+          </button>
+          <p className="text-center">
+            Already have an account ?
+            <Link to="/login" className="link text-accent cursor-pointer">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </HomeLayout>
+  );
+};
+
+export default Signup;
