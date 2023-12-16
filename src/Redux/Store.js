30617010b48355d04c3185e81ab5de0b18c80bf5
rohitdubey629 <dubeyrohit629@gmail.con>
2023-12-16 06:00:36 +0530

    Added course slice to redux

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlicesReducer from "./Slices/AuthSlice";
+import courseSliceReducer from "./Slices/CourseSlice";
 
 const store = configureStore({
   reducer: {
     auth: authSlicesReducer,
+    course: courseSliceReducer,
   },
   devTools: true,
 });
